1b82b183b41581a20bd0621456e387af7c166f5e
Saurav Kumar <103976631+Sauravkr9@users.noreply.github.com>
2023-04-06 05:33:05 +0530

    Update homepage.js

--- my-app/src/components/home/homepage.js
+++ my-app/src/components/home/homepage.js
@@ -1,10 +1,11 @@
 import React from "react"
 import "./homepage.css"
-
+import { useLocation } from "react-router-dom"
 const Homepage = ({setLoginUser}) => {
+    const location=useLocation()
     return (
         <div className="homepage">
-            <h1>Hello and welcome to our Homepage</h1>
+            <h1>Hello {location.state} welcome to our Homepage</h1>
             <div className="button" onClick={() => setLoginUser({})} >Logout</div>
         </div>
     )
